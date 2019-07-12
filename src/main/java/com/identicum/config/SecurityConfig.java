b102c5cecc37392b3622ab1017a17d7c85d7d453
ggallard <ggallard@identicum.com>
2019-07-12 10:56:34 -0300

    Logout URL with parameters to logoff OP.

--- src/main/java/com/identicum/config/SecurityConfig.java
+++ src/main/java/com/identicum/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.identicum.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,19 +15,26 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableOAuth2Sso
 @EnableOAuth2Client
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+	@Value("${idp.end_session_uri}")
+	private String end_session_uri;
 
+	@Value("${app.post_logout_uri}")
+	private String post_logout_uri;
+
+	@Override
+    public void configure(HttpSecurity http) throws Exception {
     	http.headers().frameOptions().disable();
         http.csrf().disable()
             .authorizeRequests()
         		.antMatchers("/welcome", "/webjars/**", "/css/**").permitAll()
                 .anyRequest().authenticated()
                 .and().logout()
-                	.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                	.logoutSuccessUrl("/welcome").permitAll();
-    }
+                	.logoutUrl("/logout")
+                	.invalidateHttpSession(true)
+					.logoutSuccessUrl(end_session_uri+"?post_logout_redirect_uri="+post_logout_uri);
+	}
 
 }
